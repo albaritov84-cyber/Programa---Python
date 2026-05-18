8afab7738c798961734b5efe2cc52534e31b6715
Cristiam Lopez <albaritov84@gmail.com>
2026-05-18 12:36:05 -0500

    Update and rename README.md to Fase5.py

---
+++ Fase5.py
@@ -0,0 +1,19 @@
+# Programa---Python
+
+Presentado por Cristiam Lopez 
+Universidad Nacional abierta y a distancia-UNAD 
+Vicerrectoría Académica y de Investigación 
+Unidad gestora: ECBTI 
+Programa: Ciencias Básicas 
+Curso: Fundamentos de programación 
+Código: 213022A_2201 
+
+En este  programa trata de un listado de películas  donde lleva titulo año de estreno, genero, calificación con estrellas, se le agrego un librería utilizando el  tabúlate para realizar un cuadro con columnas para una buena presentación  
+
+cumple con:
+# Mostrar la tabla con las columnas útiles para la grabación
+print(tabulate(peliculas_tabla, headers=[
+cumple con:
+# Criterios de filtrado
+min_rating = 7.0  # calificación mínima (numérica)
+min_year = 2021  # año mínimo
